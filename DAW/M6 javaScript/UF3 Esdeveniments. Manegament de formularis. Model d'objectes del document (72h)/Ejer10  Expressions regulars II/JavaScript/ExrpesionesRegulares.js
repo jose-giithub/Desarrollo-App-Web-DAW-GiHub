add3d95c1ef7b8ed6d137466073374fd04525f0b
jose-giithub <jose.rodriguez@insbaixcamp.cat>
2023-11-19 09:08:40 +0100

    Se crea el directorio general donde guardar todos mis ejercicios
    Se crea el directorio padre donde se añadirán  poco a poco todos los ejercicios que se van realizando.

---
+++ DAW/M6 javaScript/UF3 Esdeveniments. Manegament de formularis. Model d'objectes del document (72h)/Ejer10  Expressions regulars II/JavaScript/ExrpesionesRegulares.js
@@ -0,0 +1,20 @@
+//cargar el html
+
+//capturar eventos
+//clic botón inicio
+
+//capturar  la expresión regExr
+//capturar la fase que ha incluido
+
+//llamar funciones
+
+//si hace clic sobre botón inicio se muestra las el programa principal
+
+//verificar si la expresión es correcta
+
+//si la expresión regexr es correcta  mostrar un pop up que es correcta
+
+//si es incorrecta la expresión regexr es incorrecta mostrar pop up es incorrecta
+
+
+//funciones
