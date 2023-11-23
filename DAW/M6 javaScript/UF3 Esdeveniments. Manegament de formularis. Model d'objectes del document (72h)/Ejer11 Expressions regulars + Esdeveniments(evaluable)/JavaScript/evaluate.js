404a6f03383d1d813d2d72a7f9d9376738e0c3c1
jose-giithub <jose.rodriguez@insbaixcamp.cat>
2023-11-23 21:58:35 +0100

    Se empieza con la actividad
    Se crea el css y el js

---
+++ DAW/M6 javaScript/UF3 Esdeveniments. Manegament de formularis. Model d'objectes del document (72h)/Ejer11 Expressions regulars + Esdeveniments(evaluable)/JavaScript/evaluate.js
@@ -0,0 +1,16 @@
+
+
+// web para verificar https://regexr.com/
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Entro');
+
+    //capturo evento
+    var dataNacimiento = document.getElementById('inputEx1');
+    dataNacimiento.addEventListener("keydown", function(event) {
+        console.log( event.key );
+
+      });
+    
+    //llamar funciones
+});
