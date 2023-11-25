df5f06f5513c931d18176010bd8c27b8c1baf3e9
jose-giithub <jose.rodriguez@insbaixcamp.cat>
2023-11-25 19:36:25 +0100

    Se crea el cuerpo principal del ejercicio
    Se verifica que el icono se muestra correctmente

--- DAW/M6 javaScript/UF3 Esdeveniments. Manegament de formularis. Model d'objectes del document (72h)/Ejer11 Expressions regulars + Esdeveniments(evaluable)/JavaScript/evaluate.js
+++ DAW/M6 javaScript/UF3 Esdeveniments. Manegament de formularis. Model d'objectes del document (72h)/Ejer11 Expressions regulars + Esdeveniments(evaluable)/JavaScript/evaluate.js
@@ -1,16 +1,55 @@
 
+var exrpesionRegext1 = /^(0[1-9]|1[0-9]|2[0-9]|3[0-1])(?!00)$/;//empieza por 0 + del 1 al 9 o 1 + del 0 al - o 3 + del 0 al 1 pero no 00
 
 // web para verificar https://regexr.com/
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Entro');
+  console.log('Entro');
 
-    //capturo evento
-    var dataNacimiento = document.getElementById('inputEx1');
-    dataNacimiento.addEventListener("keydown", function(event) {
-        console.log( event.key );
+  //**********CAPTURAR TODOS LOS EVENTOS
+  //CAPTURA DE ICONOS
+  var icono1 = document.getElementById('checkEx1');
+  var icono2 = document.getElementById('checkEx2');
+  var icono3 = document.getElementById('checkEx3');
+  var icono4 = document.getElementById('checkEx4');
+  var icono5 = document.getElementById('checkEx5');
+  //CAPTURA DE INPUTS
+  var inputValue1 = document.getElementById('inputEx1');
+  var inputValue2 = document.getElementById('inputEx2');
+  var inputValue3 = document.getElementById('inputEx3');
+  var inputValue4 = document.getElementById('inputEx4');
+  var inputValue5 = document.getElementById('inputEx5');
 
-      });
-    
-    //llamar funciones
+  inputValue1.addEventListener("input", function() {
+    var texto = inputValue1.value;
+    console.log(texto);
+    if (exrpesionRegext1.test(texto)) {
+      console.log('Si es valida');
+      icono1.style.display = 'block';
+    }
+  //  verificarData(texto, icono1, exrpesionRegext1)
 });
+
+     //  
+
+
+    // verificarData(event.key , icono1, exrpesionRegext);
+  
+
+});
+
+/**
+ * Función que verifica si es correcto lo que ingresan en un 'formulario'
+ * @param {*} teclas lo que se escribio dentro del input
+ * @param {*} icono Si es correcto lo que se escribe pasara de none a block
+ * @param {*} exresionRegular para verificar que lo que se escribe es correcto 
+ */
+function verificarData( texto ,icono, exresionRegular) {
+
+  if (exresionRegular.test(texto)) {
+    console.log('Si es valida');
+    icono.style.display = 'block';
+
+  }
+
+}
